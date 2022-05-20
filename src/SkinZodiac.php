e4ec414f6f38b22b0eb8ad43b10c745b4e9aa98a
Colin Spence <udappia@gmail.com>
2022-05-20 08:53:07 -0600

    Resolves font issue

--- src/SkinZodiac.php
+++ src/SkinZodiac.php
@@ -26,5 +26,18 @@ class SkinZodiac extends SkinTemplate {
 		// Consider replacing with "responsive": true argument in skin.json
 		$out->addMeta( 'viewport', 'width=device-width, initial-scale=1' );
 		$out->addBodyClasses( 'text-gray-200' );
+		$out->addLink( array(
+			'rel' => 'preconnect',
+			'href' => 'https://fonts.googleapis.com',
+		) );
+		$out->addLink( array(
+			'rel' => 'preconnect',
+			'href' => 'https://fonts.gstatic.com',
+			'crossorigin' => true,
+		) );
+		$out->addLink( array(
+			'rel' => 'stylesheet',
+			'href' => 'https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;600&family=Spectral:wght@400;600&display=swap',
+		) );
 	}
 }
